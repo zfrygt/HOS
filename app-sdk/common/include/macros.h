01164b381e8218983538e88daa620d67bd28f897
Turan Murat Güvenç <murat.guvenc@pro-line.com.tr>
2016-10-23 16:10:13 +0300

    added exporting macros.h to common utility

---
+++ app-sdk/common/include/macros.h
@@ -0,0 +1,16 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#if defined(_WIN32) && defined(_MSC_VER)
+	#ifdef COMMON_EXPORTS
+		#define COMMON_EXPORT __declspec(dllexport)
+	#else
+		#define COMMON_EXPORT __declspec(dllimport)
+	#endif
+#elif 
+	#ifdef COMMON_EXPORTS
+		#define COMMON_EXPORT
+	#endif
+#endif
+
+#endif
