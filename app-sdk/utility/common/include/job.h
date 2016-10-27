4290eb2181785344b7c4c146cd0e60c39e5258a9
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-10-27 21:22:45 +0300

    changed job execute function signature

--- app-sdk/utility/common/include/job.h
+++ app-sdk/utility/common/include/job.h
@@ -1,13 +1,11 @@
 #ifndef JOB_H
 #define JOB_H
 
-class JobContext;
-
 class IJob
 {
 public:
 	virtual ~IJob() { }
-	virtual void execute(JobContext* ctx) = 0;
+	virtual void execute() = 0;
 };
 
 #endif // !JOB_H
