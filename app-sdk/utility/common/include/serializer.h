3b37224ae00adc1f7d4f189c93f851e64d623568
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-10-28 17:30:29 +0300

    added OS-dependent code

--- app-sdk/utility/common/include/serializer.h
+++ app-sdk/utility/common/include/serializer.h
@@ -13,7 +13,11 @@ public:
 		auto size = t->ByteSize();
 		assert(size > 0);
 
+#if defined(_WIN32) && defined(_MSC_VER)
         auto so = std::make_unique<SerializedObject>(size);
+#else
+        std::unique_ptr<SerializedObject> so(new SerializedObject(size));
+#endif
 		t->SerializeToArray(so->get_buf(), so->get_size());
 		return so;
 	}
@@ -22,7 +26,11 @@ public:
 	static std::unique_ptr<T> deserialize(SerializedObject* t)
 	{
         assert(t != nullptr);
+#if defined(_WIN32) && defined(_MSC_VER)
         auto temp = std::make_unique<T>();
+#else
+        std::unique_ptr<T> temp(new T);
+#endif
 		auto success = temp->ParseFromArray(t->get_buf(), t->get_size());
 		assert(success);
 		return temp;
