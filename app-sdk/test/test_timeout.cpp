75dad5ecd3c9b6790620c5a11429dbce6f666cbf
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-10-30 22:27:30 +0300

    added timeout test

--- app-sdk/test/test_timeout.cpp
+++ app-sdk/test/test_timeout.cpp
@@ -27,9 +27,9 @@ int main()
 	{
 		connector->start();
 
-        while (true)
+        while (!connector->timeout())
 		{
-            connector->heartbeat(500);
+            connector->heartbeat(50);
 		}
 	};
 
@@ -38,17 +38,6 @@ int main()
 	std::string client_name;
 	auto client_message = recv_client_message(server_socket, client_name);
 	assert(client_message->type() == Init);
-
-    std::cout << "waiting..\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(16000)); // exceed 15 seconds to timeout
-
-//    std::unique_ptr<ServerMessage> req(new ServerMessage);
-//    req->set_type(Ping);
-//    send_server_message(server_socket, req.get(), client_name);
-
-//    std::string client_name_ping;
-//    auto client = recv_client_message(server_socket, client_name_ping);
-//    assert(client->type() == Pong);
 
 	conn_feature.get();
 
