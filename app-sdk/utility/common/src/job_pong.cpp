28f8a9cdc401dc2a902865e8bdfe1209641bf7e0
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-10-31 17:05:40 +0300

    minor change

--- app-sdk/utility/common/src/job_pong.cpp
+++ app-sdk/utility/common/src/job_pong.cpp
@@ -1,22 +1,20 @@
 #include <job_pong.h>
-#include <job_context.h>
 #include <hos_protocol.pb.h>
 #include <connector.h>
-#include <iostream>
 
 JobPong::JobPong(Connector* connector):
 m_connector(connector)
 {
-	assert(m_connector);
+	assert(m_connector != nullptr);
 }
 
 JobPong::~JobPong()
 {
-	std::cout << "Destructing PongJob\n";
 }
 
 void JobPong::execute()
 {
+	assert(m_connector != nullptr);
 	ClientMessage client_message;
 	client_message.set_type(Pong);
 	m_connector->send(&client_message);
