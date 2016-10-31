4ef2d12528f46ec362cbb275601e49983b903c02
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-10-31 17:03:17 +0300

    moved common definitions to utils.h file

--- app-sdk/utility/common/include/utils.h
+++ app-sdk/utility/common/include/utils.h
@@ -7,6 +7,11 @@
 #include <memory>
 #include <chrono>
 
+#define HEARTHBEAT_INTERVAL_IN_SECONDS 5 
+#define TIMEOUT_INTERVAL_IN_SECONDS (3 * HEARTHBEAT_INTERVAL_IN_SECONDS)
+#define TIMEOUT_CHECK_INTERVAL_IN_SECONDS 2
+#include <thread>
+
 inline int64_t current_time(){
 	auto temp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 	return static_cast<int64_t>(temp) / 1000;
