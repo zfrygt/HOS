d715506e4fc15e09360696921dddab2c932ecacc
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-11-01 17:44:23 +0300

    jobs will be using client_name instead of Client objext

--- app-sdk/utility/common/src/job_ping.cpp
+++ app-sdk/utility/common/src/job_ping.cpp
@@ -2,12 +2,12 @@
 #include <hos_protocol.pb.h>
 #include <server.h>
 
-JobPing::JobPing(Server* server, Client* client):
+JobPing::JobPing(Server* server, const std::string& client_name):
 m_server(server),
-m_client(client)
+m_client_name(std::move(client_name))
 {
 	assert(m_server != nullptr);
-	assert(m_client != nullptr);
+	assert(!m_client_name.empty());
 }
 
 JobPing::~JobPing()
@@ -19,5 +19,5 @@ void JobPing::execute()
 	assert(m_server != nullptr);
 	ServerMessage server_message;
 	server_message.set_type(Ping);
-	m_server->send(m_client, &server_message);
+	m_server->send(m_client_name, &server_message);
 }
