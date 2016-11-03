f004f6edee16d684dd81f1b88b64905ed285b3cb
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-11-03 15:21:53 +0300

    receptionist program implemented

--- app-sdk/modules/receptionist/src/main.cpp
+++ app-sdk/modules/receptionist/src/main.cpp
@@ -1,5 +1,50 @@
+#include <module_connector.h>
+#include <utils.h>
+#include <tbb/concurrent_queue.h>
+#include <strategy_queue.h>
+#include <job.h>
+#include <async_job_queue.h>
+#include <job_pong.h>
+
+using spServerMessage = std::shared_ptr<ServerMessage>;
+const int MAX_JOB_COUNT = 100;
 
 int main()
 {
+	AsyncJobQueue<IJob, MAX_JOB_COUNT> q;
+
+	tbb::concurrent_bounded_queue<spServerMessage> queue;
+	queue.set_capacity(MAX_JOB_COUNT);
+
+	IStrategy *strategy = new QueueStrategy(&queue);
+
+	ModuleConnector module_base(strategy, "tcp://localhost:5555", "receptionist");
+	
+	module_base.connect();
+
+	while (true)
+	{
+		spServerMessage message;
+		queue.pop(message);
+
+		if (!message) break;
+
+		if (message)
+		{
+
+			switch (message->type())
+			{
+			case Ping:
+				q.add_job(std::make_shared<JobPong>(&module_base));
+				break;
+			case Pong: break;
+			case Init: break;
+			default: break;
+			}
+		}
+	}
+
+	delete strategy;
+
 	return 0;
 }
