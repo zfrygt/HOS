9685be0ccda2c98643685c600158d5db05e472b5
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-11-05 22:54:31 +0300

    module connector class using queue policy on receive

---
+++ app-sdk/utility/common/include/module_connector_policy.h
@@ -0,0 +1,41 @@
+#ifndef MODULE_CONNECTOR_POLICIES_H
+#define MODULE_CONNECTOR_POLICIES_H
+
+#include <future>
+#include <module_connector.h>
+
+template<typename ReceivePolicy, typename ...Args>
+class ModuleConnectorTemp : ReceivePolicy
+{
+public:
+	explicit ModuleConnectorTemp(std::shared_ptr<spdlog::logger>&& logger, const std::string url, Args&&... args) :
+		m_receive_policy(new ReceivePolicy(std::forward<Args>(args)...))
+	{
+		m_connector = std::make_unique<ModuleConnector>(m_receive_policy, std::forward<std::shared_ptr<spdlog::logger>>(logger), url.c_str(), "receptionist");
+		connect();
+	}
+
+	~ModuleConnectorTemp()
+	{
+		m_future.get();
+	}
+
+	inline ModuleConnector* get_connector() const { return m_connector.get(); }
+
+protected:
+	void connect()
+	{
+		auto future = std::async(std::launch::async, [this](){
+			m_connector->connect();
+		});
+
+		m_future = std::move(future);
+	}
+
+private:
+	ReceivePolicy* m_receive_policy;
+	std::unique_ptr<ModuleConnector> m_connector;
+	std::future<void> m_future;
+};
+
+#endif
