64530e332c70133e238ed6d6b461b9c169fca8fa
Turan Murat Güvenç <t.murat.guvenc@gmail.com>
2016-11-08 10:01:42 +0300

    minor bug again fixed

--- app-sdk/modules/receptionist/src/job_hostinfo.cpp
+++ app-sdk/modules/receptionist/src/job_hostinfo.cpp
@@ -15,5 +15,7 @@ void JobHostInfo::execute()
 	ClientMessage client_message;
 	client_message.set_type(ClientMessage_Type_HostInfo);
 	client_message.mutable_host_info()->set_total_cpu(HardwareStatus::get_total_cpu());
+	client_message.mutable_host_info()->set_total_ram(HardwareStatus::get_total_mem());
+	client_message.mutable_host_info()->set_total_disk(HardwareStatus::get_total_dsk());
 	m_module_connector->send(&client_message);
 }
